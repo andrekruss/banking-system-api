7c9391874821fc1d484e6a9d12531437f2782830
Andre Kruss <andrebeltramekruss@gmail.com>
2025-05-28 20:46:45 -0300

    Added CORS bypass

--- backend/src/app.py
+++ backend/src/app.py
@@ -3,6 +3,7 @@ from beanie import init_beanie
 from contextlib import asynccontextmanager
 from dotenv import load_dotenv
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from motor.motor_asyncio import AsyncIOMotorClient
 
 from src.database.models.account import Account
@@ -30,6 +31,14 @@ async def lifespan(app: FastAPI):
     print("Shutting down app...")
 
 app = FastAPI(lifespan=lifespan)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],  
+    allow_headers=["*"],  
+)
 
 app.include_router(user_router)
 app.include_router(auth_router)
